29d020359e1cccdeef741acba158021316930536
ProfJAT <JoshuaT22@lakesideschool.org>
2022-06-16 04:00:56 -0700

    Prep for Heroku deployment

--- syd_nook/syd_nook/settings.py
+++ syd_nook/syd_nook/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR.joinpath('static')
 ]
