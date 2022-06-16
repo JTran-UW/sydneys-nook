d502c79c12c80aec444b3979b891a35222a7123d
ProfJAT <JoshuaT22@lakesideschool.org>
2022-06-16 04:13:35 -0700

    Modified wsgi

--- syd_nook/syd_nook/wsgi.py
+++ syd_nook/syd_nook/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'syd_nook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'syd_nook.syd_nook.settings')
 
 application = get_wsgi_application()
