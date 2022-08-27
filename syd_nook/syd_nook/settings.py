b0dce1450e039170d60dc00ad99d77c92ec0fc03
Joshua Tran <46096425+ProfJAT@users.noreply.github.com>
2022-08-27 00:36:34 -0700

    Update settings.py

--- syd_nook/syd_nook/settings.py
+++ syd_nook/syd_nook/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^j=r@253b0m7*jl%x%q%b=#nm_)!2%4=z%=_j%=$lev^qn3=!4'
+SECRET_KEY = 'INSERT SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
